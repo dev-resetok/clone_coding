f363023faad70c60e22f6253dd5696df8e39a593
seungmin <746aa1357@gmail.com>
2024-08-07 17:08:58 +0900

    2024_08_07

---
+++ naver/join/join.js
@@ -0,0 +1,39 @@
+const eye = document.getElementById("eye_toggle");
+const radios = document.querySelectorAll(".radio_item");
+const eye_image = document.querySelector('img[src="images/eye.png"]');
+const male = document.getElementById("gender1");
+const female = document.getElementById("gender2");
+const unknown = document.getElementById("gender3");
+const list_male = document.getElementById("list_male");
+const list_female = document.getElementById("list_female");
+const list_unknown = document.getElementById("list_unknown");
+
+eye.addEventListener("change", () => {
+    if (eye.checked) {
+        eye_image.src = "images/eye2.png";
+    } else {
+        eye_image.src = "images/eye.png";
+    }
+});
+
+radios.forEach((radio) => {
+    radio.addEventListener("change", (e) => {
+        if (male.checked) {
+            list_male.className = "radio_item_checked";
+            list_female.className = "radio_item";
+            list_unknown.className = "radio_item";
+        } else if (female.checked) {
+            list_female.className = "radio_item_checked";
+            list_unknown.className = "radio_item";
+            list_male.className = "radio_item";
+        } else if (unknown.checked) {
+            list_unknown.className = "radio_item_checked";
+            list_male.className = "radio_item";
+            list_female.className = "radio_item";
+        } else {
+            list_male.className = "radio_item";
+            list_female.className = "radio_item";
+            list_unknown.className = "radio_item";
+        }
+    });
+});
