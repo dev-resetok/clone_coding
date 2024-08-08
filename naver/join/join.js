4ba77f4d56a9bb1f94bccaad43addb8b409431e1
seungmin <746aa1357@gmail.com>
2024-08-08 21:08:33 +0900

    2024_08_08

--- naver/join/join.js
+++ naver/join/join.js
@@ -1,3 +1,7 @@
+const checkbox = document.getElementById("join_toggle");
+const checkbox_image = document.querySelector(
+    'img[src="images/uncheckedbox.png"]'
+);
 const eye = document.getElementById("eye_toggle");
 const radios = document.querySelectorAll(".radio_item");
 const eye_image = document.querySelector('img[src="images/eye.png"]');
@@ -7,6 +11,14 @@ const unknown = document.getElementById("gender3");
 const list_male = document.getElementById("list_male");
 const list_female = document.getElementById("list_female");
 const list_unknown = document.getElementById("list_unknown");
+
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        checkbox_image.src = "images/checkedbox.png";
+    } else {
+        checkbox_image.src = "images/uncheckedbox.png";
+    }
+});
 
 eye.addEventListener("change", () => {
     if (eye.checked) {
